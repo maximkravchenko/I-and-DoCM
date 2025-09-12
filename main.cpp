e5824b702cf492c35b86e021a2e6a7bf8e7f6e77
Maxim_NB_XGL15.6 <saliterbrason@gmail.com>
2025-09-12 09:54:26 +0300

    add prog

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    w.setFixedSize(1100, 700);
+    w.setWindowTitle("Grimm pressent");
+
+    w.show();
+    return a.exec();
+}
